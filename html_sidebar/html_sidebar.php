b4d4ef5af478c0bcd69a0212743b0f12e57783e7
Jitendra <jitendrasahu17996@gmail.com>
2023-10-02 15:02:15 +0530

    html sidebar using css

---
+++ html_sidebar/html_sidebar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Html Sidebar</title>
+    <link rel="stylesheet" href="style.css" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar">
+            <h2>Sidebar</h2>
+            <ul>
+                <li><a href="#"><span class="material-icons">home</span>&nbsp;Home</a></li>
+                <li><a href="#"><span class="material-icons">person</span>&nbsp;Profile</a></li>
+                <li><a href="#"><span class="material-icons">roundabout_left</span>&nbsp;About</a></li>
+                <li><a href="#"><span class="material-icons">rss_feed</span>&nbsp;Blogs</a></li>
+                <li><a href="#"><span class="material-icons">phone</span>&nbsp;Contact</a></li>
+                <li><a href="#"><span class="material-icons">map</span>&nbsp;Map</a></li>
+            </ul>
+            <div class="bottom_navigation">
+                <a href="#"><span class="material-icons">facebook</span></a>
+                <a href="#"><span class="material-icons">instagram</span></a>
+                <a href="#"><span class="material-icons">logout</span></a>
+            </div>
+        </div>
+        <div class="main_content">
+            <div class="main_card">
+                <div class="header">Welcome, have a nice day</div>
+                <div class="info">
+                    <h3>Lorem ipsum</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
